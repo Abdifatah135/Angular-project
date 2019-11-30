4029201ac368c204792719ffa8f49b56c86879c2
Abdi <oxabdi75@gmail.com>
2019-12-01 02:52:49 +0300

    added a pipe

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,12 +7,12 @@ import { Quote } from "../quote";
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, "No Human is Limed", "Eliud Kipchoge"),
-    new Quote(2, "Be a Voice not an Echo", "Nelson Mandela"),
-    new Quote(3, "Love For All, Hatred For None", "Romeo"),
-    new Quote(4, "Change the world by being yourself", "Malcom X"),
-    new Quote(5, "Every moment is a fresh beggining", "Angelina Davies"),
-    new Quote(6, "Aspire to inspire before you expire", "Thomas Michael")
+    new Quote(1, "No Human is Limed", "Eliud Kipchoge",new Date(2019,12,1)),
+    new Quote(2, "Be a Voice not an Echo", "Nelson Mandela",new Date(2019,12,1)),
+    new Quote(3, "Love For All, Hatred For None", "Romeo",new Date(2019,12,1)),
+    new Quote(4, "Change the world by being yourself", "Malcom X",new Date(2019,12,1)),
+    new Quote(5, "Every moment is a fresh beggining", "Angelina Davies",new Date(2019,12,1)),
+    new Quote(6, "Aspire to inspire before you expire", "Thomas Michael",new Date(2019,12,1))
   ];
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription
