83f8d0bfc837f5aa2471d05e0dfb71caa812466f
Abdi <oxabdi75@gmail.com>
2019-12-01 01:48:36 +0300

    added a toggle button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Quote } from '../quote';
+import { Component, OnInit } from "@angular/core";
+import { Quote } from "../quote";
 @Component({
-  selector: 'app-quote',
-  templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  selector: "app-quote",
+  templateUrl: "./quote.component.html",
+  styleUrls: ["./quote.component.css"]
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, "No Human is Limed",'Eliud Kipchoge' ),
-    new Quote(2,"Be a Voice not an Echo",'Nelson Mandela' ),
-    new Quote(3, "Love For All, Hatred For None" ,'Romeo'),
-    new Quote(4,"Change the world by being yourself" ,'Malcom X'),
-    new Quote(5,"Every moment is a fresh beggining" ,'Angelina Davies'),
-    new Quote(6, "Aspire to inspire before you expire",'Thomas Michael' )  ];
-  constructor() { }
-
-  ngOnInit() {
+    new Quote(1, "No Human is Limed", "Eliud Kipchoge"),
+    new Quote(2, "Be a Voice not an Echo", "Nelson Mandela"),
+    new Quote(3, "Love For All, Hatred For None", "Romeo"),
+    new Quote(4, "Change the world by being yourself", "Malcom X"),
+    new Quote(5, "Every moment is a fresh beggining", "Angelina Davies"),
+    new Quote(6, "Aspire to inspire before you expire", "Thomas Michael")
+  ];
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription
   }
+  constructor() {}
 
+  ngOnInit() {}
 }
