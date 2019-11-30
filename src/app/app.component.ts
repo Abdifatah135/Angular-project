a9814e2fe22dcc23ccdb24948534570801fac0ec
Abdi <oxabdi75@gmail.com>
2019-12-01 00:45:07 +0300

    added id and name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { Quote } from "./quote";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  quotes:string[];
+  quotes: Quote[] = [
+    { id: 1, name: "No Human is Limed" },
+    { id: 2, name: "Be a Voice not an Echo" },
+    { id: 3, name: "Love For All, Hatred For None" },
+    { id: 4, name: "Change the world by being yourself" },
+    { id: 5, name: "Every moment is a fresh beggining" },
+    { id: 6, name: "Aspire to inspire before you expire" }
+  ];
 
-  constructor(){
-    this.quotes = ['No Human is Limites', 'Attitude is th First step of Winning', 'Be a voice not an echo']
-  } 
+  constructor() {}
 }
