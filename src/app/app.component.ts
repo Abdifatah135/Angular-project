64f0d1cb30f9f260efb94ab8c6336474886f7772
Abdi <oxabdi75@gmail.com>
2019-12-01 01:19:35 +0300

    added another component called quote-detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,7 @@ import { Quote } from "./quote";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  quotes: Quote[] = [
-    { id: 1, name: "No Human is Limed" },
-    { id: 2, name: "Be a Voice not an Echo" },
-    { id: 3, name: "Love For All, Hatred For None" },
-    { id: 4, name: "Change the world by being yourself" },
-    { id: 5, name: "Every moment is a fresh beggining" },
-    { id: 6, name: "Aspire to inspire before you expire" }
-  ];
+  
 
   constructor() {}
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes: Quote[] = [
+    { id: 1, name: "No Human is Limed",description:'Eliud Kipchoge' },
+    { id: 2, name: "Be a Voice not an Echo",description:'Nelson Mandela' },
+    { id: 3, name: "Love For All, Hatred For None" ,description:'Romeo'},
+    { id: 4, name: "Change the world by being yourself" ,description:'Malcom X'},
+    { id: 5, name: "Every moment is a fresh beggining" ,description:'Angelina Davies'},
+    { id: 6, name: "Aspire to inspire before you expire",description:'Thomas Michael' }
+  ];
   constructor() { }
 
   ngOnInit() {
