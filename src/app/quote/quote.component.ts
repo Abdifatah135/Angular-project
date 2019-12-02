fec14b36d7f58a913474c86999e47b6f9c9845b1
Abdi <oxabdi75@gmail.com>
2019-12-02 16:33:26 +0300

    added changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,7 +7,7 @@ import { Quote } from "../quote";
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, "No Human is Limed", "Eliud Kipchoge",new Date(2018,6,1)),
+    new Quote(1, "Eliud Kipchoge","No Human is Limed", new Date(2018,6,1)),
     new Quote(2, "Be a Voice not an Echo", "Nelson Mandela",new Date(2010,10,23)),
     new Quote(3, "Love For All, Hatred For None", "Romeo",new Date(2017,1,1)),
     new Quote(4, "Change the world by being yourself", "Malcom X",new Date(2019,12,1)),
